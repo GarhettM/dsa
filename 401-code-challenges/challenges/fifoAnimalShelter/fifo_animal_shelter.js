3f8d6ff6ade04dece320d05ddada17d9e3092813
Garhett Morgan <32fossil@gmail.com>
2020-06-29 23:11:34 -0700

    challenge 16

---
+++ 401-code-challenges/challenges/fifoAnimalShelter/fifo_animal_shelter.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Node {
+  constructor(value)  {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class AnimalShelter {
+  constructor() {
+    this.head = null;
+  }
+}
