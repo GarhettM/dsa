35216f2490a4488a7838b8c9e4cb08eb09f2fb71
Garhett Morgan <32fossil@gmail.com>
2020-09-16 17:48:30 -0700

    challenge 3 done

---
+++ 401-code-challenges/javaChallenges/src/main/java/javaChallenges/BinarySearch.java
@@ -0,0 +1,22 @@
+package javaChallenges;
+
+public class BinarySearch {
+    public int binarySearch(int[] arr, int a, int z, int n) {
+
+        if(a <= z) {
+            int mid = (a + z) / 2;
+            System.out.println(mid);
+            if (arr[mid] == n) {
+                return mid;
+            }
+
+            if (arr[mid] > n) {
+                return binarySearch(arr, a, mid - 1, n);
+            }
+            if (arr[mid] < n) {
+                return binarySearch(arr, mid + 1, z, n);
+            }
+        }
+        return -1;
+    }
+}
