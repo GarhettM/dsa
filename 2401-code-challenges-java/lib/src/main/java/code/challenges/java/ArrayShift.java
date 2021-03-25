d747929845300ac95cee267e8c9eb0bdc3e0db08
Garhett <garhett.l.morgan@gmail.com>
2021-03-24 18:23:18 -0700

    code challenge 1

--- 2401-code-challenges-java/lib/src/main/java/code/challenges/java/ArrayShift.java
+++ 2401-code-challenges-java/lib/src/main/java/code/challenges/java/ArrayShift.java
@@ -1,6 +1,29 @@
 package code.challenges.java;
 
+import java.util.ArrayList;
+
 public class ArrayShift {
 
-    public 
+    public ArrayList<Integer> ShiftArray(ArrayList<Integer> arr, int n) {
+        ArrayList<Integer> newArr = new ArrayList<>();
+        boolean toggle = false;
+        int j;
+
+        for (int i = 0; i < arr.size(); i++) {
+            if(n < arr.get(i)) {
+                newArr.add(arr.get(i));
+                continue;
+            }
+            newArr.add(n);
+            j = i + 1;
+            break;
+        }
+
+        for(int k = j; j < arr.size(); k++) {
+            newArr.add(arr.get(k));
+        }
+
+        return arr;
+    }
+
 }
