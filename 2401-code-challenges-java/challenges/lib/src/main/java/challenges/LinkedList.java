8df6c934ee9d2c9da88fdb188ccfacf30717168e
Garhett <garhett.l.morgan@gmail.com>
2021-03-31 15:12:29 -0700

    finished challenge 7

--- 2401-code-challenges-java/challenges/lib/src/main/java/challenges/LinkedList.java
+++ 2401-code-challenges-java/challenges/lib/src/main/java/challenges/LinkedList.java
@@ -87,6 +87,46 @@ public class LinkedList {
         System.out.println(finalString);
         return finalString;
     }
+
+    public int kthFromEnd(int k) {
+        int counter = k;
+        Node current1 = this.head;
+        Node current2 = this.head;
+
+        while(current1 != null) {
+            if(counter > 0) {
+                current1 = current1.next;
+                counter--;
+                continue;
+            }
+            current1 = current1.next;
+            current2 = current2.next;
+        }
+        if(current2 == null) {
+            return -1;
+        }
+        return current2.value;
+    }
+
+    public Node llZip(LinkedList ll1, LinkedList ll2) {
+        Node current1 = ll1.head;
+        Node current2 = ll2.head;
+
+        while(current1 != null && current2 != null) {
+            Node temp2 = current2.next;
+            Node temp1 = current1.next;
+            current1.next = current2;
+            current1.next.next = temp1;
+            current2 = temp2;
+            if(current1.next.next != null) {
+                current1 = current1.next.next;
+            } else {
+                current1.next.next = current2;
+                break;
+            }
+        }
+        return ll1.head;
+    }
 }
 
 class Node {
--- 2401-code-challenges-java/challenges/lib/src/test/java/challenges/LinkedListTest.java
+++ 2401-code-challenges-java/challenges/lib/src/test/java/challenges/LinkedListTest.java
@@ -68,4 +68,48 @@ public class LinkedListTest {
         testLL.insertAfter(3, 4);
         assertEquals("HEAD ->  8 ->  6 ->  4 ->  3 ->  2 ->  NULL", testLL.toString());
     }
+
+    @Test
+    public void kthFromTheEndTest() {
+        LinkedList testLL = new LinkedList();
+        testLL.insert(2);
+        testLL.insert(4);
+        testLL.insert(6);
+        testLL.insert(8);
+        assertEquals(4, testLL.kthFromEnd(2));
+    }
+
+    @Test
+    public void llZipTest() {
+        LinkedList testLL1 = new LinkedList();
+        testLL1.insert(0);
+        testLL1.insert(1);
+        testLL1.insert(3);
+        testLL1.insert(5);
+        testLL1.insert(7);
+        testLL1.insert(9);
+
+        LinkedList testLL2 = new LinkedList();
+        testLL2.insert(2);
+        testLL2.insert(4);
+        testLL2.insert(6);
+        testLL2.insert(8);
+        testLL1.llZip(testLL1, testLL2);
+
+        assertEquals("HEAD ->  9 ->  8 ->  7 ->  6 ->  5 ->  4 ->  3 ->  2 ->  1 ->  0 ->  NULL", testLL1.toString());
+
+        LinkedList testLL3 = new LinkedList();
+        testLL3.insert(7);
+        testLL3.insert(9);
+
+        LinkedList testLL4 = new LinkedList();
+        testLL4.insert(2);
+        testLL4.insert(4);
+        testLL4.insert(6);
+        testLL4.insert(8);
+        testLL3.llZip(testLL3, testLL4);
+
+        assertEquals("HEAD ->  9 ->  8 ->  7 ->  6 ->  4 ->  2 ->  NULL", testLL3.toString());
+
+    }
 }
